1de899897210f41ce2d9c56a6a44de17a51442cb
odan <odan3240@gmail.com>
2018-10-05 19:02:59 +0900

    add editorconfig

--- aws/index.ts
+++ aws/index.ts
@@ -1,65 +1,65 @@
 import cdk = require('@aws-cdk/cdk');
 import s3 = require('@aws-cdk/aws-s3');
 import cloudfront = require('@aws-cdk/aws-cloudfront');
- 
+
 class StaticSiteStack extends cdk.Stack {
-    constructor(parent: cdk.App, id: string, props?: cdk.StackProps) {
-        super(parent, id, props);
+  constructor(parent: cdk.App, id: string, props?: cdk.StackProps) {
+    super(parent, id, props);
 
-        const bucketName = 'aws-cdk-sandbox-static-site';
+    const bucketName = 'aws-cdk-sandbox-static-site';
 
-        const bucket = new s3.Bucket(this, bucketName, {
-            bucketName
-        });
-
-
-        const oaiResource = new cloudfront.cloudformation.CloudFrontOriginAccessIdentityResource(this, 'staticSiteOAIResource', {
-            cloudFrontOriginAccessIdentityConfig: {
-                comment: 'static site'
-            }
-        })
-
-        new s3.cloudformation.BucketPolicyResource(this, 'staticSitePolicyResource', {
-            bucket: bucket.bucketName,
-            policyDocument: {
-                Statement: [
-                    {
-                        Sid: 'cdn',
-                        Effect: 'Allow',
-                        Principal: {
-                            AWS: `arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${oaiResource.cloudFrontOriginAccessIdentityId}`
-                        },
-                        Action: [
-                            's3:GetObject'
-                        ],
-                        Resource: [
-                            `arn:aws:s3:::${bucket.bucketName}/*`
-                        ]
-                    }
-                ]
-            }
-        })
+    const bucket = new s3.Bucket(this, bucketName, {
+      bucketName
+    });
 
 
-        new cloudfront.CloudFrontWebDistribution(this, 'staticSiteDistribution', {
-            originConfigs: [
-                {
-                    s3OriginSource: {
-                        s3BucketSource: bucket,
-                        originAccessIdentity: oaiResource,
-                    },
-                    behaviors : [ {isDefaultBehavior: true} ]
-                }
+    const oaiResource = new cloudfront.cloudformation.CloudFrontOriginAccessIdentityResource(this, 'staticSiteOAIResource', {
+      cloudFrontOriginAccessIdentityConfig: {
+        comment: 'static site'
+      }
+    })
+
+    new s3.cloudformation.BucketPolicyResource(this, 'staticSitePolicyResource', {
+      bucket: bucket.bucketName,
+      policyDocument: {
+        Statement: [
+          {
+            Sid: 'cdn',
+            Effect: 'Allow',
+            Principal: {
+              AWS: `arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${oaiResource.cloudFrontOriginAccessIdentityId}`
+            },
+            Action: [
+              's3:GetObject'
+            ],
+            Resource: [
+              `arn:aws:s3:::${bucket.bucketName}/*`
             ]
-        })
-    }
+          }
+        ]
+      }
+    })
+
+
+    new cloudfront.CloudFrontWebDistribution(this, 'staticSiteDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: oaiResource,
+          },
+          behaviors: [{ isDefaultBehavior: true }]
+        }
+      ]
+    })
+  }
 }
 class StaticSiteApp extends cdk.App {
-    constructor(argv: string[]) {
-        super(argv);
+  constructor(argv: string[]) {
+    super(argv);
 
-        new StaticSiteStack(this, 'static-site-stack')
-    }
+    new StaticSiteStack(this, 'static-site-stack')
+  }
 }
- 
+
 process.stdout.write(new StaticSiteApp(process.argv).run());
